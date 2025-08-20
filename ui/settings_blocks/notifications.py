2b9383c5faf8853fec5a32c36d8858af4e9ef5d6
Oruj Gafarov <65281943+orrstudio@users.noreply.github.com>
2025-08-20 04:04:01 +0400

    Уведомления: вкладки. активная зеленная

--- ui/settings_blocks/notifications.py
+++ ui/settings_blocks/notifications.py
@@ -20,8 +20,8 @@ def create_notifications_section(settings_window):
     container = GridLayout(
         cols=1,
         size_hint=(1, None),
-        height=dp(1500),  # Увеличиваем высоту контейнера для отображения всех элементов с отступами
-        padding=(dp(30), dp(5), dp(30), dp(5)),
+        height=dp(100),  # Увеличиваем высоту контейнера для отображения всех элементов с отступами
+        padding=(dp(10), dp(5), dp(10), dp(5)),
         spacing=dp(5)
     )
 
@@ -33,7 +33,7 @@ def create_notifications_section(settings_window):
     )
     
     # Вычисляем высоту таблицы на основе количества строк
-    row_height = dp(50)  # Высота одной строки
+    row_height = dp(70)  # Высота одной строки
     num_rows = 10  # Общее количество строк
     table.height = row_height * num_rows
 
@@ -51,36 +51,26 @@ def create_notifications_section(settings_window):
     
     # Данные для строк
     rows = [
-        ("Visual Notification", 'visual_switch', '28sp', True, 0, dp(35)),
+        ("Visual Notification", 'visual_switch', '28sp', True, 0, dp(60)),
         ("           At Adhan", 'visual_switch_at_adhan', '22sp', False, 1, dp(30)),
         ("           15 min before", 'visual_switch_15_min', '22sp', False, 1, dp(30)),
         ("           30 min before", 'visual_switch_30_min', '22sp', False, 1, dp(30)),
         ("           45 min before", 'visual_switch_45_min', '22sp', False, 1, dp(30)),
         ("           60 min before", 'visual_switch_60_min', '22sp', False, 1, dp(30)),
-        ("           ", None, '12sp', False, 1, dp(10)),
-        ("Voice Notification", 'voice_switch', '28sp', True, 0, dp(35)),
+        ("Voice Notification", 'voice_switch', '28sp', True, 0, dp(60)),
         ("           At Adhan", 'voice_switch_at_adhan', '22sp', False, 1, dp(30)),
         ("           15 min before", 'voice_switch_15_min', '22sp', False, 1, dp(30)),
         ("           30 min before", 'voice_switch_30_min', '22sp', False, 1, dp(30)),
         ("           45 min before", 'voice_switch_45_min', '22sp', False, 1, dp(30)),
         ("           60 min before", 'voice_switch_60_min', '22sp', False, 1, dp(30)),
-        ("           ", None, '12sp', False, 1, dp(10)),
-        ("Play Adhan", 'switch_play_adhan', '28sp', True, 0, dp(35)),
+        ("Play Adhan", 'switch_play_adhan', '28sp', True, 0, dp(60)),
         ("           Fajr", 'switch_fajr', '22sp', False, 1, dp(30)),
         ("           Dhuhr", 'switch_dhuhr', '22sp', False, 1, dp(30)),
         ("           Asr", 'switch_asr', '22sp', False, 1, dp(30)),
         ("           Maghrib", 'switch_maghrib', '22sp', False, 1, dp(30)),
         ("           Isha", 'switch_isha', '22sp', False, 1, dp(30)),
-        ("           ", None, '12sp', False, 1, dp(10)),
-        ("Choose Muezzin", None, '28sp', True, 0, dp(35)),  # Без переключателя
+        ("Choose Muezzin", None, '28sp', True, 0, dp(60)),  # Без переключателя
         ("           Default Adhan", 'switch_default_adhan', '22sp', False, 1, dp(30)),
-        ("           Ahmed Al Nufais", 'switch_ahmed_al_nufais', '22sp', False, 1, dp(30)),
-        ("           Mansour Al Zahrani", 'switch_mansour_al_zahrani', '22sp', False, 1, dp(30)),
-        ("           Mehdi Yarrahi Fajr", 'switch_mehdi_yarrahi_fajr', '22sp', False, 1, dp(30)),
-        ("           Mihr Com", 'switch_mihr_com', '22sp', False, 1, dp(30)),
-        ("           Mishary Rashid Alafasy", 'switch_mishary_rashid_alafasy', '22sp', False, 1, dp(30)),
-        ("           Mishary Rashid Alafasy Fajr", 'switch_mishary_rashid_alafasy_fajr', '22sp', False, 1, dp(30)),
-        ("           Old Adhan", 'switch_old_adhan', '22sp', False, 1, dp(30)),
     ]
 
     for text, switch_attr, font_size, is_bold, _, switch_height in rows:
@@ -97,7 +87,7 @@ def create_notifications_section(settings_window):
         )
 
         def update_text_size(inst, val):
-            padding = dp(30)  # ваш желаемый отступ слева
+            padding = dp(10)  # ваш желаемый отступ слева
             inst.text_size = (val[0] - padding, None)  # уменьшаем ширину на padding
             inst.canvas.ask_update()
 
@@ -109,7 +99,7 @@ def create_notifications_section(settings_window):
         if switch_attr is not None:
             switch_layout = AnchorLayout(
                 anchor_x='center',
-                anchor_y='top',
+                anchor_y='center',
                 size_hint=(0.2, None),
                 height=switch_height
             )
